830555ebaa875e2b10af172f808b509b913d5e72
Tiago Marques <simplesmente_moi@hotmail.com>
2018-11-12 22:00:04 +0100

    Add project files.

---
+++ MSTODOclone/Helpers/ToDoItemTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace MSTODOclone.Helpers {
+    class ToDoItemTemplateSelector : DataTemplateSelector {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container) {
+            var lv = GetListView(container);
+            if (lv != null) {
+                var i = lv.Items.IndexOf(item);
+                if (i < lv.Items.Count - 1) {
+                    return GeneralToDoItemTemplate;
+                }
+
+                return NewToDoItemTemplate;
+                
+            }
+            return NewToDoItemTemplate;
+        }
+
+        public DataTemplate GeneralToDoItemTemplate { get; set; }
+        public DataTemplate NewToDoItemTemplate { get; set; }
+
+        public static ListView GetListView(DependencyObject element) {
+            var parent = VisualTreeHelper.GetParent(element);
+                if (parent == null) {
+                return null;
+            }
+            var parentListView = parent as ListView;
+                return parentListView ?? GetListView(parent);
+        }
+    }
+}
---
+++ MSTODOclone/Model/ToDoModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTODOclone {
+
+    public class TodoItem {
+
+        private string _name;
+        private bool _isDone;
+
+
+        public string Name {
+            get => _name;
+            set {
+                _name = value;
+            }
+        }
+
+        public bool IsDone {
+            get; set;
+        }
+
+        public TodoItem() {
+
+        }
+
+    }
+
+    public class EmptyTodoItem : TodoItem {
+        public bool IsSaved;
+    }
+
+
+}
